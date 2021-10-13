dafd82daf5a1200dd0a5cf19d31cd01ff0aa8cf1
GabrielEduardus <gabrieleduardus@github.com>
2021-10-13 23:37:54 +0700

    Website YBike

---
+++ YBike/Website/JS/home.js
@@ -0,0 +1,51 @@
+const prev = $('#prev')
+const next = $('#next')
+const imagesContainer = $('.images')
+const images = $('.images div')
+
+
+const IMAGE_WIDTH = 350
+
+let currentImageIndex = 0
+let currentPosition = 0
+
+function main(){
+    setPrevHandler();
+    setNextHandler();
+}
+
+function setPrevHandler(){
+    prev.on('click', () => {
+        currentImageIndex--
+
+        if(currentImageIndex < 0){
+            currentImageIndex = images.length - 1
+            currentPosition = -IMAGE_WIDTH * currentImageIndex
+        }else {
+            currentPosition += IMAGE_WIDTH
+        }
+        updateImageSlider()
+    })
+}
+
+function setNextHandler(){
+    next.on('click', () => {
+        currentImageIndex++
+
+        if (currentImageIndex >= images.length) {
+            currentImageIndex = 0
+            currentPosition = 0
+        }else {
+            currentPosition -= IMAGE_WIDTH;
+        }
+
+        updateImageSlider()
+    })
+}
+
+function updateImageSlider(){
+    const prevElem = images.eq(0);
+    prevElem.animate({ marginLeft: currentPosition })
+}
+
+main()
